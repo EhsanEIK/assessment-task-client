8824f6b885d27f690362952dd87d79d3228e105d
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-12-21 22:27:29 +0600

    loaded users data from db and showed them in Table.js

--- src/Pages/Table/Table.js
+++ src/Pages/Table/Table.js
@@ -1,13 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const Table = () => {
+    const { data: users = [] } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/users');
+            const data = await res.json();
+            return data;
+        }
+    })
+
     return (
         <div className="overflow-x-auto mt-8">
             <h1 className='text-3xl font-semibold text-center mb-8'>Table Data</h1>
             <table className="table w-full">
                 <thead>
                     <tr>
-                        <th></th>
+                        <th>ID</th>
                         <th>Name</th>
                         <th>DOB</th>
                         <th>Email</th>
@@ -15,13 +25,16 @@ const Table = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="hover">
-                        <th>2</th>
-                        <td>Hart Hagerty</td>
-                        <td>Desktop Support Technician</td>
-                        <td>Purple</td>
-                        <td>Purple</td>
-                    </tr>
+                    {
+                        users.map((user, idx) =>
+                            <tr className="hover">
+                                <th>{idx + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.dob}</td>
+                                <td>{user.email}</td>
+                                <td>{user.phone}</td>
+                            </tr>)
+                    }
                 </tbody>
             </table>
         </div>
